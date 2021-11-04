b71ecd5e26e322d35d27729e9391a307a6e1d9fd
lenggar <lenggar@badr.interactive.com>
2021-11-04 10:26:33 +0700

    edit lumache

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SMILE UNDP'
+copyright = '2021, UNDP'
+author = 'UNDP'
 
 release = '0.1'
 version = '0.1.0'
